b9e4d296fb46e0fde8dc678e90c88e3a89081751
arthur teixeira <arthurcarvalho24@gmail.com>
2021-05-18 21:57:17 -0300

    Adicionado funcionalidade de upload e download de currículo

--- routes/User/MemberRoutes.js
+++ routes/User/MemberRoutes.js
@@ -3,14 +3,17 @@ const Router = require("koa-router")
 const router = new Router();
 
 const CrudFactory = require('../../factories/CrudFactory')
+const UploadFactory = require('../../factories/UploadFactory')
 const MemberController = require('../../controllers/User/MemberController')
 
 router
 .post('/add-evaluation', MemberController.addEvaluation)
 .post('/member',(ctx)=> CrudFactory.create('Member', ctx))
+.post('/member-file', UploadFactory.single('curriculum'), MemberController.UpdateFile)
+.get('/member-file/:id', MemberController.SendFile)
 .get('/members', (ctx)=> CrudFactory.getAll('Member', ctx)) 
 .get('/member/:id', (ctx)=> CrudFactory.getById('Member', ctx)) 
-.patch('/member', (ctx)=> CrudFactory.update('Member', ctx))
+.patch('/member', UploadFactory.single('curriculum'), (ctx)=> CrudFactory.update('Member', ctx))
 .delete('/member/:id',(ctx)=>  CrudFactory.delete('Member', ctx))
 
 module.exports = router.routes()
