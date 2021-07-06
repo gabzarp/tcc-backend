349a605cac105b28afc133be926d2533f3719c5c
Gabriel <gabriel.zarpelon@dealersites.com.br>
2021-07-06 01:26:54 -0300

    ffinal

--- controllers/Integrations/Trello.js
+++ controllers/Integrations/Trello.js
@@ -5,7 +5,7 @@ module.exports = {
         try {
             const trelloProject = await axios.post(`https://api.trello.com/1/boards/?key=${process.env.TRELLO_API}&token=${process.env.TRELLO_TOKEN}&name=${project.name}`);
             console.log(trelloProject)
-            const trelloExternalSources = new ExternalSources({name: 'Trello', link: trelloProject.data.url, sourceId: trelloProject.data.id})
+            const trelloExternalSources = new ExternalSources({name: 'Trello', link: trelloProject.data.url, sourceId: trelloProject.data.id, idOrganization: 'redtcc'})
             trelloExternalSources.save()
             project.externalSources.push(trelloExternalSources)
             project.save()
--- models/Project/Risks.js
+++ models/Project/Risks.js
@@ -3,7 +3,8 @@ const mongoose = require("../../database/mongodb.js");
 const Risks = new mongoose.Schema({
     name: { type: String, required: true},
     description: { type: String, required: true},
-    project: {type: mongoose.Schema.Types.ObjectId, ref:"Project", required:true}
+    project: {type: mongoose.Schema.Types.ObjectId, ref:"Project", required:true},
+    date: {type: Date, required: true}
 });
 
 module.exports = mongoose.model("Risks", Risks);
